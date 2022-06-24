86f3d76296397e172795fb13e1859cbbe13c1360
Pedro Carrasco <72097375+dropeedro@users.noreply.github.com>
2022-06-24 10:34:46 -0400

    Delete student directory

--- student/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conexion = mysqli_connect('localhost','root','','asistencia_prueba') or die('Cannot connect to server');
-//mysql_select_db('asistencia') or die ('Cannot found database');
-
-?>
